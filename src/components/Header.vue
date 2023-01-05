15cbc5c82db0f5493a045228ce7e6e26d292f887
hbsongk <68498323+hbsongk@users.noreply.github.com>
2023-01-05 19:53:09 +0900

    feat(fe): add skeleton

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>Memo App</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  background: #f7f7f7;
+  border-top: 1px solid #eee;
+  padding: 25px;
+}
+</style>
---
+++ src/components/Memo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="memo">
+    <ul>
+      <li>메모 1 내용</li>
+      <li>메모 2 내용</li>
+      <li>메모 3 내용</li>
+      <li>메모 4 내용</li>
+      <li>메모 5 내용</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.memo ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
